28f6531a708eec0488cc6af4024edad757a4235b
chenpeng <chenpengsmail@qq.com>
2017-11-30 17:52:13 +0800

    can be compiled

---
+++ net/DCommunicator.cpp
@@ -0,0 +1,13 @@
+
+#include "dagger/Dagger.hpp"
+
+namespace Dagger {
+
+    DCommunicator *DCommunicator::GetInstance() {
+        return nullptr;
+    }
+    DNetConnection *DCommunicator::BuildConnection(const DNetAddr &addr) {
+        return nullptr;
+    }
+
+}
